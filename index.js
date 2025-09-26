da86a3d6fa1c058339054db1415fe86b93ebc761
vidu-bit <viduvtr@gmail.com>
2025-09-26 18:28:46 +0200

    Add middleware to parse JSON bodies

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//Middleware to parse JSON bodies
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
