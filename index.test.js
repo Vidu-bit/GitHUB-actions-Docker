46fe989e41f8a3c843e24d96277951cf25eb72d8
vidu-bit <viduvtr@gmail.com>
2025-09-26 18:35:01 +0200

    Fix index.test.js

--- index.test.js
+++ index.test.js
@@ -1,30 +1,23 @@
 const request = require('supertest');
 const { app, close } = require('./index');
 
-describe('GET /', () => {
-    it('should return Hello World!', async () => {
+describe('Test all endpoint API', ()=> {
+    it('GET / should return Hello World', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
         expect(res.text).toBe('Hello World!');
-    }
+    });
 
-);
-}
-);
-
-describe('GET /health', () => {
-    it('should return OK', async () => {
+    it('GET /health should return OK', async () => {
         const res = await request(app).get('/health');
         expect(res.statusCode).toEqual(200);
         expect(res.text).toBe('OK');
-    }
-    
-);
-}
+    }); 
 
-);
-
-afterAll((done) => {
+    afterAll((done) => {
     close();
     done();
 });
+});
+
+
