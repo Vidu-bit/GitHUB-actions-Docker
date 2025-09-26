98dbc27ec5f0d8d8b277d71da6bd4783bbb20e9c
vidu-bit <viduvtr@gmail.com>
2025-09-26 18:41:46 +0200

    Some ch 2

--- index.test.js
+++ index.test.js
@@ -5,13 +5,13 @@ describe('Test all endpoint API', ()=> {
     it('GET / should return Hello World', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
-        expect(res.text).toBe('Hello World!');
+        expect(res.body).toEqual('Hello World!');
     });
 
     it('GET /health should return OK', async () => {
         const res = await request(app).get('/health');
         expect(res.statusCode).toEqual(200);
-        expect(res.text).toBe('OK');
+        expect(res.body).toEqual('OK');
     }); 
 
     afterAll((done) => {
